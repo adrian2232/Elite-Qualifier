1588c68065a8c990df87db497f1f64390cd797fa
Adrian Rose <adrian.rose@stu.pfisd.net>
2021-01-12 22:28:37 +0000

    names

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Elite qualifier Project
+
+background
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-pip3 install beautifulsoup4
+
 from bs4 import BeautifulSoup
 URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
 page = requests.get(URL)
@@ -6,5 +6,12 @@ soup = BeautifulSoup(page.content,'html.parser')
 results = soup.find(id='ResultsContainer')
 print(results.prettify())
 job_elems = results.find_all('section', class_='card_content')
-for job_elem in job_elems
+for job_elem in job_elems:
   print(job_elem, end= '/n'*2)
+  title_elem = job_elem.find('h2', class_='title')
+company_elem = job_elem.find('div', class_='company')
+location_elem = job_elem.find('div', class_='location')
+print(title_elem)
+print(company_elem)
+print(location_elem)
+print()
