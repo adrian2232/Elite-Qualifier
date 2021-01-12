eb31fd03ea31c7bb99b27bcc3d91b56601b073cb
Adrian Rose <adrian.rose@stu.pfisd.net>
2021-01-12 22:20:14 +0000

    initial readme

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ soup = BeautifulSoup(page.content,'html.parser')
 results = soup.find(id='ResultsContainer')
 print(results.prettify())
 job_elems = results.find_all('section', class_='card_content')
+for job_elem in job_elems
+  print(job_elem, end= '/n'*2)
