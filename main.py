d7c2ab6f1a7c81978fc2b6027de2db22bcd2f04a
Adrian Rose <adrian.rose@stu.pfisd.net>
2021-01-14 22:12:51 +0000

    even more code

--- main.py
+++ main.py
@@ -9,16 +9,26 @@ job_elems = results.find_all('section', class_='card_content')
 for job_elem in job_elems:
   print(job_elem, end= '/n'*2)
   title_elem = job_elem.find('h2', class_='title')
-company_elem = job_elem.find('div', class_='company')
-location_elem = job_elem.find('div', class_='location')
-print(title_elem)
-print(company_elem)
-print(location_elem)
-print()
-if None in (title_elem, company_elem, location_elem):
+  company_elem = job_elem.find('div', class_='company')
+  location_elem = job_elem.find('div', class_='location')
+  print(title_elem)
+  print(company_elem)
+  print(location_elem)
+  print()
+for job_elem in job_elems:
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
         continue
     print(title_elem.text.strip())
     print(company_elem.text.strip())
     print(location_elem.text.strip())
     print()
 python_jobs = results.find_all('h2', string='Python Developer')
+python_jobs = results.find_all('h2',string=lambda text: 'python' in text.lower())
+print(len(python_jobs))
+for p_job in python_jobs:
+    link = p_job.find('a')['href']
+    print(p_job.text.strip())
+    print(f"Apply here: {link}\n")
